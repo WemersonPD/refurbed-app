5e1236553417c250ae98300ecf24862301d46ed9
WemersonPD <wemersondev@gmail.com>
2026-03-16 01:35:15 +0000

    fix: adjust Button component height for consistent styling

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="w-full rounded-xl border border-gray-300 bg-white py-4 text-lg font-medium shadow-sm hover:bg-gray-50">
+    <button class="w-full rounded-xl border border-gray-300 bg-white text-lg font-medium shadow-sm hover:bg-gray-50 h-9">
         <slot />
     </button>
 </template>
